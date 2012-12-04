04be38e0a8dbe1ecf600048e42f1156cfe65a0fc
Matt MacDonald <matt.macdonald@gmail.com>
2012-12-04 08:58:01 -0500

    basic ui

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatilikeaboutyou::Application.config.secret_token = 'ebfe15ee105e0471748ef33b9e02a1097504ead765f0d29941c4f3c4e3b4de36ce9adbbbf5e27f14087e38078805b680585a29b9488727d108dff33ab7add18f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Whatilikeaboutyou::Application.config.session_store :cookie_store, key: '_whatilikeaboutyou_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Whatilikeaboutyou::Application.config.session_store :active_record_store
